d5a153f22bc135db431c498da563b1429ce8384d
Andre Bocchini <andrebocchini@gmail.com>
2016-02-11 17:46:51 -0600

    Updated thread sorting with a few other sorting possibilities

--- SwiftChatty/Common Models/Mappable/ThreadType.swift
+++ SwiftChatty/Common Models/Mappable/ThreadType.swift
@@ -8,8 +8,18 @@
 
 public enum ThreadSortOrder {
 
-    case NewestFirst
-    case OldestFirst
+    /// All posts sorted by date only, with the newer ones first
+    case FlatNewestFirst
+    /// All posts sorted by date only, with the older ones first
+    case FlatOldestFirst
+    /// All replies directly below the root post sorted by date with the newer ones first, 
+    /// and all of the replies below those organized by threads with the older
+    /// ones first.  The root post is always the first post in the thread.
+    case ThreadedNewestFirst
+    /// All replies directly below the root post sorted by date with the older ones first,
+    /// and all of the replies below those organized by threads with the older
+    /// ones first.  The root post is always the first post in the thread.
+    case ThreadedOldestFirst
 
 }
 
@@ -27,8 +37,12 @@ extension ThreadType {
         return [Post]()
     }
 
-    public func rootPost() -> Post? {
-        return (self.posts.filter { $0.id == self.id  }).first
+    public func rootPost() -> Post {
+        if let rootPost = (self.posts.filter { $0.id == self.id  }).first {
+            return rootPost
+        } else {
+            return Post()
+        }
     }
     /// Calculates how many subthreads deep a particular post is
     ///
@@ -55,7 +69,8 @@ extension ThreadType {
     public func parent(forPost post: Post) -> Post? {
         return (self.posts.filter { $0.id == post.parentId }).first
     }
-    /// Finds all of the posts that were made after a certain post
+    /// Finds all of the posts that were made after a certain post in the same thread (or any of its 
+    /// subthreads)
     ///
     /// - parameter post: The post whose descendants we want to find
     /// - returns: An array of posts containing any descendants found
@@ -70,17 +85,81 @@ extension ThreadType {
 
         return descendantPosts
     }
+    /// Finds all posts immediately below (in a hierarchical thread) another post.
+    ///
+    /// - parameter posts: The list of posts to search through
+    /// - parameter post: The post under which we want to find the top level posts
+    /// - returns: An array of posts consisting of the top level posts under the post passed in as
+    ///   a parameter.
+    public func topLevelPosts(posts: [Post], underPost root: Post) -> [Post] {
+        var topLevelPosts = [Post]()
+
+        for post in posts {
+            if post.parentId == root.id {
+                topLevelPosts.append(post)
+            }
+        }
+
+        return topLevelPosts
+    }
 
     public mutating func sort(order: ThreadSortOrder) {
-        if order == .NewestFirst {
-            self.posts.sortInPlace {
-                return $0.newer(than: $1)
-            }
-        } else {
+        switch(order) {
+        case .FlatNewestFirst: fallthrough
+        case .FlatOldestFirst:
+            self.flatSort(order)
+        case .ThreadedNewestFirst: fallthrough
+        case .ThreadedOldestFirst:
+            self.threadedSort(order)
+        }
+    }
+
+    private mutating func flatSort(order: ThreadSortOrder) {
+        if (order == .ThreadedOldestFirst) {
             self.posts.sortInPlace {
                 return $0.older(than: $1)
             }
+        } else {
+            self.posts.sortInPlace {
+                return $0.newer(than: $1)
+            }
         }
+    }
+
+    private mutating func threadedSort(order: ThreadSortOrder) {
+        let postsArrayWithoutRootPost = self.posts.filter( { $0.id != self.id } )
+        let rootPost = self.rootPost()
+
+        var threadedPosts = [Post]()
+        var topLevelPosts = self.topLevelPosts(postsArrayWithoutRootPost, underPost: rootPost)
+
+        if (order == .ThreadedOldestFirst) {
+            topLevelPosts.sortInPlace( { return $0.older(than: $1) } )
+        } else {
+            topLevelPosts.sortInPlace( { return $0.newer(than: $1) } )
+        }
+
+        threadedPosts.append(rootPost)
+        for topLevelPost in topLevelPosts {
+            threadedPosts.append(topLevelPost)
+            threadedPosts.appendContentsOf(orderedThread(postsArrayWithoutRootPost, root: topLevelPost))
+        }
+
+        self.posts = threadedPosts
+    }
+
+    private func orderedThread(posts: [Post], root: Post) -> [Post] {
+        var topLevelPosts = self.topLevelPosts(posts, underPost: root)
+        var result = [Post]()
+
+        topLevelPosts.sortInPlace( { $0.older(than: $1) } )
+
+        for topLevelPost in topLevelPosts {
+            result.append(topLevelPost)
+            result.appendContentsOf(orderedThread(posts, root: topLevelPost))
+        }
+
+        return result
     }
 
 }
